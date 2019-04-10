96e2b97a3844aac77b91b64bfc7cafc5e76e828b
Ben Kirby <benkrby@gmail.com>
2019-04-10 13:38:46 -0700

    added FB integration to list screen.

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import ItemComponent from '../components/ItemComponent';
+
+import { db } from '../config';
+
+let productsRef = db.ref('/products');
 
 export default class List extends Component {
+  state = {
+    products: []
+  };
+
+  componentDidMount() {
+    productsRef.on('value', snapshop => {
+      let data = snapshot.val();
+      let products = Object.values(data);
+      this.setState({ products });
+    });
+  }
+
+
   render() {
     return (
-      <View>
-        <Text>List Rendering</Text>
+      <View style={styles.container}>
+        {this.state.products.length > 0 ? (
+          <ItemComponent products={this.state.products} />
+        ) : (
+          <Text>No Products :(</Text>
+        )}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ebebeb'
+  }
+});
