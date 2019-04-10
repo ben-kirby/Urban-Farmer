7896ced476ab113e84b770cb52a876f489b5c1ef
Ben Kirby <benkrby@gmail.com>
2019-04-10 11:42:21 -0700

    Added AddItem and List screens

---
+++ src/screens/AddItem.js
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default class AddItem extends Component {
+  render(){
+    return(
+      <View>
+        <Text>AddItem Rendering</Text>
+      </View>
+    );
+  }
+}
