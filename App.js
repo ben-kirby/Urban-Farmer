23adef222ec49fd626b67c04d764ba1c051ba4c1
Ben Kirby <benkrby@gmail.com>
2019-04-10 12:18:15 -0700

    push for tanvi

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react';
+import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import React, {
-  Component
-} from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TextInput
-} from 'react-native';
-import Home from './src/screens/Home'
+import Home from './src/screens/Home';
+import AddItem from './src/screens/AddItem';
+import List from './src/screens/List';
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: Home
+  }
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render() {
+    return <AppContainer/>;
+  }
+}
 
 const styles = StyleSheet.create({
   page: {
@@ -28,15 +30,3 @@ const styles = StyleSheet.create({
 
   }
 });
-
-
-
-export default class App extends Component {
-  render() {
-    return ( 
-    <View style={styles.page}>
-      <Home/>
-    </View>
-    );
-  }
-}
