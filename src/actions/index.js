93fe5b2265e482c06ea715742c197bc01d8ca353
Ben Kirby <benkrby@gmail.com>
2019-04-10 11:34:58 -0700

    infrastructure updates from package-lock and xcode automated changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,7 @@ import Firebase from 'firebase';
 const { firebaseConfig } = constants;
 
 Firebase.initializeApp(firebaseConfig);
+
+export function getAllRecords() {
+  return function(dispatch) {}
+}
