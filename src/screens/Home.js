5b3ffe5c7f91b959aae5554f6eb1bb336fdb6701
Ben Kirby <benkrby@gmail.com>
2019-04-12 11:39:44 -0700

    eslint and react plugin added. Test run successful

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,21 +3,21 @@ import { View, Text, Button } from 'react-native';
 
 
 export default class Home extends Component {
-  render () {
-    return(
-      <View>
-        <Text>Home</Text>
-        <Button
-          title="Add Item"
-          onPress={() => this.props.navigation.navigate('AddItem')}
-        />
-        <Button
-          title="Item List"
-          color="green"
-          onPress={() => this.props.navigation.navigate('List')}
-        />
+	render () {
+		return(
+			<View>
+				<Text>Home</Text>
+				<Button
+					title="Add Item"
+					onPress={() => this.props.navigation.navigate('AddItem')}
+				/>
+				<Button
+					title="Item List"
+					color="green"
+					onPress={() => this.props.navigation.navigate('List')}
+				/>
          
-      </View>
-    )
-  }
+			</View>
+		);
+	}
 }
