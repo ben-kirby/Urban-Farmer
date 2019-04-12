ed1e069fb546f369dfd3497724b7f13577551e0d
Intern <intern@DevNows-MacBook-Pro-2.local>
2019-04-12 09:33:46 -0700

    added footer component from firebase might not use due to using react navigation for the footer but going to keep it untill we decide

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
+import { Container, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
 
 
 export default class FooterComponent extends Component {
   render() {
     return (
       <Container>
-        <Header />
+       
         <Content />
         <Footer>
           <FooterTab>
-            <Button vertical active
-            onPress={() => this.props.navigation.navigate('Home')}
-            >
+            <Button vertical active >
               <Icon name="home" />
               <Text>Home</Text>
             </Button>
+
             <Button vertical active>
               <Icon name="paper" />
               <Text>Inv</Text>
             </Button>
+
             <Button vertical active >
               <Icon name="add" />
               
