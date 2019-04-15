7e8c3b8280b2c9fab619ebf219c02321af0ab9fe
Ben Kirby <benkrby@gmail.com>
2019-04-15 13:35:51 -0700

    added forms and button to handle input. Also created function to create user in FB.

---
+++ src/screens/CreateUserScreen.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+	View,
+	TextInput,
+	Button,
+	StyleSheet,
+	Text
+} from 'react-native';
+import { auth } from '../config';
+
+export default class CreateUser extends Component {
+	state = {
+		email: '',
+		password: '',
+		uuid: null
+	};
+
+	handleSubmit = () => {
+		auth.createUserWithEmailAndPassword(this.state.email, this.state.password).then()
+	}
+
+
+
+	render() {
+		return(
+			<View style={styles.container}>
+				<TextInput
+					style={styles.input}
+					onChangeText={(text) => this.setState({email: text})}
+					placeholder="E-Mail"
+				/>
+				<TextInput
+					style={styles.input}
+					onChangeText={(text) => this.setState({password:text})}
+					secureTextEntry={true}
+					placeholder="Password"
+				/>
+				<Button
+					onPress={this.handleSubmit}
+					title="Submit"
+					color="#841584"
+				/>
+				<Text>{this.state.uuid}</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	input: {
+		width: 250,
+		margin: 5
+	},
+	container: {
+		padding: 30,
+		marginTop: 65,
+		alignItems: 'center'
+	}
+});
