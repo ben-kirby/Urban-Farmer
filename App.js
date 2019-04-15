b2ba7814cfd72825f74d3d76aea08acfe4e1eb88
Robert Lee <bobbydwise1@hotmail.com>
2019-04-15 14:12:15 -0700

    Attempt to insert switch navigator.

--- App.js
+++ App.js
@@ -1,29 +1,43 @@
 import React, {Component} from 'react';
 import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { createSwitchNavigator, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+
 
 import Home from './src/screens/Home';
 import AddItem from './src/screens/AddItem';
 import InventoryList from './src/screens/InventoryList';
 import SignInScreen from './src/screens/SignInScreen';
 
-const TabNavigator = createBottomTabNavigator(
+const HeaderNavigator = createMaterialTopTabNavigator(
   {
-    Home,
-    AddItem,
-    InventoryList,
-    SignInScreen
+    Home: {screen: Home},
   },
   {
-    initialRouteName: 'Home'
+    initialRouteName: 'Home',
+  }
+)
+// const FooterNavigator = createMaterialBottomTabNavigator(
+const FooterNavigator = createStackNavigator(
+  {
+    Home: {screen: Home},
+    AddItem: {screen: AddItem},
+    InventoryList: {screen: InventoryList},
+    SignInScreen: {screen: SignInScreen},
+  },
+  {
+    initialRouteName: 'Home',
+    activeColor: '#f0edf6',
+    inactiveColor: '#3e2465',
+    barStyle: { backgroundColor: '#694fad' },
   }
 );
 
-const AppContainer = createAppContainer(TabNavigator);
+const AppContainer = createAppContainer(FooterNavigator);
 
 export default class App extends Component {
   render() {
-    return <AppContainer/>;
+    return (<AppContainer/>);
   }
 }
 
