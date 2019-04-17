cd58547e52f7939da34e9b9139998ef8a78dcb8a
Ben Kirby <benkrby@gmail.com>
2019-04-17 13:44:59 -0700

    added itemdetail screen

---
+++ src/screens/ItemDetailScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default class ItemDetailScreen extends Component {
+	render() {
+		return(
+			
+		);
+	}
+}
