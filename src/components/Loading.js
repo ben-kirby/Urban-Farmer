227d78f8a2dc5e2195faca79c552d0c7dfcfa049
Ben Kirby <benkrby@gmail.com>
2019-04-22 14:05:51 -0700

    simple spinning carrot animation for loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import { Dimensions, Animated, View, Text, Image, StyleSheet } from 'react-native';
+import { Animated, View, Image, StyleSheet, Easing } from 'react-native';
 import Carrot from '../img/Carrot.png';
 
+
+
 export default class Loading extends Component {
+	constructor () {
+		super();
+		this.spinValue = new Animated.Value(0);
+	}
+
+	componentDidMount(){
+		this.spin();
+	}
+
+	spin () {
+		this.spinValue.setValue(0);
+		Animated.timing(
+			this.spinValue,
+			{
+				toValue: 1,
+				duration: 4000,
+				easing: Easing.linear
+			}
+		).start(() => this.spin());
+	}
+
 	render() {
+		const spin = this.spinValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: ['0deg', '360deg']
+		});
+
 		return(
 			<View style={styles.container}>
-				<Image
+				<Animated.Image
 					resizeMode='contain'
+					style={{
+						flex: 1,
+						transform: [{rotate: spin}],
+						height: undefined,
+						width: undefined
+					}}
 					source={Carrot}
-					style={styles.image}
 				/>
 			</View>
 		);
@@ -19,11 +52,12 @@ export default class Loading extends Component {
 
 const styles = StyleSheet.create({
 	container: {
-		height: 140,
+		flex: 1,
+		height: 140
 	},
 	image: {
 		flex: 1,
 		height: undefined,
 		width: undefined
-	}
+	},
 });
