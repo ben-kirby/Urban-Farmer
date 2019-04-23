6661c5659138c6c7676aa1f8d3eba170f106aa42
Robert Lee <bobbydwise1@hotmail.com>
2019-04-23 15:41:50 -0700

    Add UI input error handling.

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -12,65 +12,81 @@ export default class SignInScreen extends Component {
     password: ''
   };
 
+  handleEmail = (text) => {
+    this.setState({ email: text })
+  };
 
-handleSubmit = () => {
-  auth.signInWithEmailAndPassword(this.state.email, this.state.password).then(response => {
-    if (response.user) {
-      this.storeData('uid', response.user.uid).then(() => {
-        this.props.navigation.navigate('AppStack');
-      })
-    } else {
-      alert('Oops! There was a problem with that. Try again plz.');
-    }
-  });
-}
+  handlePassword = (text) => {
+    this.setState({ password: text })
+  };
 
-storeData = async (key, value) => {
-  try {
-    await AsyncStorage.setItem(key, value)
-  } catch (e) {
-    Alert.alert(e.message)
+  handleSubmit = () => {
+    auth.signInWithEmailAndPassword(this.state.email, this.state.password).then(response => {
+      if (response.user) {
+        this.storeData('uid', response.user.uid).then(() => {
+          this.props.navigation.navigate('AppStack');
+        })
+      } else {
+        alert('Oops! There was a problem with that. Try again plz.');
+      }
+    });
   }
-}
+
+
+  storeData = async (key, value) => {
+    try {
+      await AsyncStorage.setItem(key, value)
+    } catch (e) {
+      Alert.alert(e.message)
+    }
+  }
 
   render() {
     return (
       <View style={styles.container}>
-      <Text style={{fontWeight: 'bold', fontSize: 24}}>URBAN FARMER</Text>
-      <Text>Login</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => this.setState({email: text})}
-        placeholder="E-Mail"
-        value={this.state.text}
-        textContextType='emailAddress'
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => this.setState({password:text})}
-        secureTextEntry={true}
-        placeholder="Password"
-        value={this.state.text} 
-        textContextType='password'
-      />
-      <Button
-        onPress={this.handleSubmit}
-        title="Sign In"
-        color="#4a822f"
-      />
-    <Text>{'\nNot an existing user?\n'}</Text>
-      <Button
-        onPress={() => this.props.navigation.navigate('CreateUser')}
-        title="Sign Up"
-        color="#4a822f"
-      />
-    <Text>{'\n'}</Text>
-      <Button
-        onPress={() => this.props.navigation.navigate('AppStack')}
-        title="*Shortcut to Main App"
-        color="#4a822f"
-      />
-    </View>
+        <Text style={{fontWeight: 'bold', fontSize: 24}}>URBAN FARMER</Text>
+        <Text>Login</Text>
+        <TextInput
+          underlineColorAndroid = 'transparent'
+          style={styles.input}
+          onChangeText={this.handleEmail}
+          placeholder="E-Mail"
+          autoCapitalize='none'
+          value={this.state.text}
+          textContextType='emailAddress'
+          keyboardType='email-address'
+          maxLength={255}
+          />
+        <TextInput
+          underlineColorAndroid = 'transparent'
+          style={styles.input}
+          onChangeText={this.handlePassword}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize='none'
+          value={this.state.text}
+          textContextType='password'
+          keyboardType='default'
+          maxLength={32}
+          />
+        <Button
+          onPress={this.handleSubmit}
+          title="Sign In"
+          color="#4a822f"
+          />
+        <Text>{'\nNot an existing user?\n'}</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('CreateUser')}
+          title="Sign Up"
+          color="#4a822f"
+          />
+        <Text>{'\n'}</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate('AppStack')}
+          title="*Shortcut to Main App"
+          color="#4a822f"
+          />
+      </View>
     );
   }
 }
