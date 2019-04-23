90c09618a27d48666dc98ec7ef5ed66a87ea94a4
tanvi <tanvi.garg23@gmail.com>
2019-04-23 16:03:53 -0700

    form validation for all inputs and clear input on submit

--- src/screens/AddItemScreen.js
+++ src/screens/AddItemScreen.js
@@ -26,8 +26,13 @@ export default class AddItemScreen extends Component {
     name: '',
     price: '',
     quantity: '',
-    uid: ''
+    uid: '',
+    errorQty: false,
+    errorPrice: false,
+    errorName: false,
+    submitValid: true
   };
+
 
  async getUserId(){
    try {
@@ -44,15 +49,46 @@ export default class AddItemScreen extends Component {
   componentDidMount() {
     this.getUserId();
   }
+  
+  handleChangeName = (text) => {
+    const reg = /^[a-zA-Z]+$/;
+    let correctName = text.match(reg) ? this.setState({submitValid: true, errorName: false}) : this.setState({errorName: true,  submitValid: false});
+    this.setState({name:text});
+  }
+
+  handleChangePrice = (text) => {
+    const reg = /^[+]?([1-9][0-9]*(?:[\.][0-9]*)?|0*\.0*[1-9][0-9]*)(?:[eE][+-][0-9]+)?$/;
+    let correctPrice = text.match(reg) ? this.setState({submitValid: true, errorPrice: false}) : this.setState({errorPrice: true, submitValid: false});
+    this.setState({price:text});
+  }
+  
+  handleChangeQuantity = (text) => {
+    const reg = /^[1-9]\d*$/;
+    let correctEntry = text.match(reg) ? this.setState({submitValid: true, errorQty: false}) : this.setState({errorQty: true, submitValid: false});
+    this.setState({quantity:text});
+  }   
 
   handleSubmit = () => {
     console.log(readData)
-    addItem(this.state.name, this.state.price, this.state.quantity, this.state.uid);
-    console.log("handle submit triggered")
-    alert('item saved!');
+    if(this.state.submitValid){
+      addItem(this.state.name, this.state.price, this.state.quantity, this.state.uid);
+      this.priceInputRef.clear();
+      console.log("handle submit triggered");
+      alert('item saved!');
+    }
   };
 
+
+
   render(){
+    let errorQtyVisible;
+    let errorNameVisible;
+    let errorPriceVisible;
+    let errorSubmitVisible;
+    this.state.errorName ? (errorNameVisible = <Text>letters only, no numbers and special characters</Text>) : null;
+    this.state.errorPrice ? (errorPriceVisible = <Text>numbers only, no text and special characters</Text>) : null;
+    this.state.errorQty ? (errorQtyVisible = <Text>please enter a number</Text>) : null;
+    (this.state.submitValid === false) ? (errorSubmitVisible = <Text>please correct the inputs</Text>) : null;
     return(
      
       <View style={styles.main}>
@@ -60,27 +96,32 @@ export default class AddItemScreen extends Component {
       <Text style={styles.title}>Add Item</Text>
       <TextInput
         style={styles.itemInput}
-        onChangeText={(text) => this.setState({name:text})}
+        onChangeText={this.handleChangeName}
         placeholder='Item name'
       />
+      {errorNameVisible}
       <TextInput
+        ref={ref => this.priceInputRef = ref}
         style={styles.itemInput}
-        onChangeText={(text) => this.setState({price:text})}
+        value={this.state.price}
+        onChangeText={this.handleChangePrice}
         placeholder='Item price'
       />
+      {errorPriceVisible}
       <TextInput
         style={styles.itemInput}
-        onChangeText={(text) => this.setState({quantity:text})}
+        onChangeText={this.handleChangeQuantity}
         placeholder='Item quantity'
       />
+      {errorQtyVisible}
       <TouchableHighlight
         style={styles.button}
         
         onPress={this.handleSubmit}
-
       >
-        <Text style={styles.buttonText}>Add</Text>
+       <Text style={styles.buttonText}>Add</Text>
       </TouchableHighlight>
+      {errorSubmitVisible}
 
     </View>
 
