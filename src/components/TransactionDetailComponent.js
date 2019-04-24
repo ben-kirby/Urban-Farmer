15cba4f09da8d5a67088884a082f43a71a825b0e
Ben Kirby <benkrby@gmail.com>
2019-04-24 13:08:34 -0700

    Created Transaction Detail component

---
+++ src/components/TransactionDetailComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class TransactionDetail extends Component {
+	render() {
+		return(
+			<View>
+				
+			</View>
+		);
+	}
+}
