1a3bc65e786ea42a42c7ac4d9faada578e9e2ad9
Ben Kirby <benkrby@gmail.com>
2019-04-24 14:42:18 -0700

    working on formatting data

--- src/components/TransactionDetailComponent.js
+++ src/components/TransactionDetailComponent.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Conatainer, Header, Content, Accordion, Container } from 'native-base';
 
 export default class TransactionDetail extends Component {
+	state = {
+		dataArray: []
+	}
+	componentDidMount() {
+		if (this.state.dataArray.length > 0) {
+			this.formatTransactionData();
+		}
+	}
+
+	formatTransactionData = () => {
+		let formattedDataArray = [];
+		for (let i = 0; i < this.props.transactionData.length; i++) {
+			let time = Date.toString(this.props.transactionData[i].timestamp)
+			let formattedDataElement = {
+				title: time,
+				content: "Test Content"
+			}
+			formattedDataArray.push(formattedDataElement)
+		}
+		this.setState({
+			dataArray: formattedDataArray
+		});
+	}
+
+
 	render() {
 		return(
-			<View>
-				
-			</View>
+			<Container>
+				<Header/>
+				<Content padder>
+					<Accordion 
+						dataArray={this.state.dataArray}
+						expanded={0}
+					/>
+				</Content>
+			</Container>
 		);
 	}
 }
