49892de3a00b77ba97d2de8beb11a8389fcde2ab
Ben Kirby <benkrby@gmail.com>
2019-04-24 13:27:01 -0700

    sales data pulls by uid now

--- src/screens/SalesDetailScreen.js
+++ src/screens/SalesDetailScreen.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
+import { auth, db } from '../config';
+
+
 import styles from '../styles/stylesComponent';
 
 export default class ItemDetailScreen extends Component {
+	state = {
+		transactions: []
+	}
+
+	componentDidMount() {
+		let uid = auth.currentUser.uid;
+		db.ref("transactions/" + uid).on("value", snapshot => {
+			let transaction = snapshot.val();
+			let data = [];
+			{
+				Object.keys(transaction).map(index => {
+					data.push({
+						id: index,
+						uid: uid,
+						itemName: transaction[index].item,
+						qtySold: transaction[index].qtySold,
+						unitPrice: transaction[index].unitPrice,
+						timestamp: transaction[index].timestamp
+					});
+				});
+			}
+			this.setState({
+				transactions: Object.values(data)
+			});			
+		})
+	}
+
 	render() {
 		return (
 			<View>
-				<Text>Sales Screen</Text>
+				<ScrollView>
+					<Text>Sales Screen</Text>
+				</ScrollView>
 			</View>
 		);
 	}
