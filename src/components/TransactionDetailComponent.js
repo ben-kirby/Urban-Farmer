92c619ca514982a4706c25c40ec3255bfce4c458
Ben Kirby <benkrby@gmail.com>
2019-04-25 10:16:18 -0700

    added function to format date

--- src/components/TransactionDetailComponent.js
+++ src/components/TransactionDetailComponent.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Conatainer, Header, Content, Accordion, Container } from 'native-base';
+import { View, Text } from 'react-native';
+import { Header, Content, Accordion, Container } from 'native-base';
 
 export default class TransactionDetail extends Component {
 	state = {
 		dataArray: []
 	}
 	componentDidMount() {
-		if (this.state.dataArray.length > 0) {
+		
+		if (this.state.dataArray.length == 0) {
 			this.formatTransactionData();
 		}
 	}
@@ -15,27 +16,37 @@ export default class TransactionDetail extends Component {
 	formatTransactionData = () => {
 		let formattedDataArray = [];
 		for (let i = 0; i < this.props.transactionData.length; i++) {
-			let time = Date.toString(this.props.transactionData[i].timestamp)
+			this.formatDate(this.props.transactionData[i].timestamp)
+			
 			let formattedDataElement = {
 				title: time,
 				content: "Test Content"
 			}
-			formattedDataArray.push(formattedDataElement)
+			formattedDataArray.push(formattedDataElement);
 		}
 		this.setState({
 			dataArray: formattedDataArray
 		});
+		
+	}
+
+	formatDate = (timestamp) => {
+		let date = new Date(timestamp);
+		let month = date.getMonth();
+		let day = date.getDate()
+		let time = date.toString()
+		console.log(time);
+		
 	}
 
 
 	render() {
 		return(
 			<Container>
-				<Header/>
+				<Text>Details Loading</Text>
 				<Content padder>
 					<Accordion 
 						dataArray={this.state.dataArray}
-						expanded={0}
 					/>
 				</Content>
 			</Container>
