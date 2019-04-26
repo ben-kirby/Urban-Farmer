16fe3d5f836c1b86327fbf48a4684ffa860279ae
Ben Kirby <benkrby@gmail.com>
2019-04-26 10:29:49 -0700

    refactored to accept a single product object

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -1,73 +1,44 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView, RefreshControl } from 'react-native';
-import PropTypes from 'prop-types';
+import { View, Text, StyleSheet } from 'react-native';
 import SoldModal from './SoldModal';
 import EditModal from './EditModal';
+import PropTypes from 'prop-types';
+
 
 
 
 export default class ItemComponent extends Component {
-  state = {
-    refreshing: false
-  }
+	static propTypes = {
+		products: PropTypes.object
+	};
 
-  static propTypes = {
-    products: PropTypes.array.isRequired
-  };
-
-  _onRefresh = () => {
-    this.setState({refreshing: true});
-    this.props.refresh().then(() => {
-      this.setState({refreshing: false})
-    });
-  }
-
-  render() {
-    return(
-      <ScrollView 
-        style={StyleSheet.itemsList}
-        refreshControl={
-          <RefreshControl
-            refreshing={this.state.refreshing}
-            onRefresh={this._onRefresh}
-          />
-        }
-      >
-        {this.props.products.map((item, index) => {
-          return (
-            <View style={styles.itemCard} key={index}>
-              <Text style={styles.itemtext}>Product Name: {item.name} </Text>
-              
-              <EditModal item={item}/>
-              <SoldModal item={item}/>
-             
-            </View>
-          );
-        })}
-      </ScrollView>
-    );
-  }
+	render() {
+		return(
+			<View style={styles.itemCard}>
+				<Text style={styles.itemtext}>{this.props.product.name} </Text>
+				<EditModal item={this.props.product}/>
+				<SoldModal item={this.props.product}/>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  itemsList: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-around'
-  },
-  itemtext: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    borderWidth:2,
-    borderColor: 'black'
-  },
-  itemCard: {
-
-    marginTop:10,
-    marginRight:10,
-    marginLeft:10,
-    backgroundColor :'#f9f4eb'
-  }
-
+	itemsList: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-around'
+	},
+	itemtext: {
+		fontSize: 24,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		marginTop: 10
+	},
+	itemCard: {
+		marginTop:10,
+		marginRight:10,
+		marginLeft:10,
+		backgroundColor :'#f9f4eb'
+	}
 });
