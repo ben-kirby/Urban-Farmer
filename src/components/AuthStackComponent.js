fcaf8c755fc9b5b38a84e5715d2985e17e65a11c
Ben Kirby <benkrby@gmail.com>
2019-05-18 14:37:56 -0700

    Authstack now is just one screen with conditional rendering

--- src/components/AuthStackComponent.js
+++ src/components/AuthStackComponent.js
@@ -2,25 +2,22 @@ import React, {Component} from 'react';
 import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { createSwitchNavigator, navigationOptions } from 'react-navigation';
 
-import SignInScreen from '../screens/SignInScreen';
-import CreateUserScreen from '../screens/CreateUserScreen';
+import SignInScreen from './SignInComponent';
+import CreateUserScreen from './CreateUserComponent';
+import UserAuthScreen from '../screens/UserAuthScreen';
 
 const RouterConfig =
   {
-    SignIn:
+  	UserAuth:
       {
-        screen: SignInScreen,
-      },
-    CreateUser:
-      {
-        screen: CreateUserScreen,
-      },
-  }
+      	screen: UserAuthScreen,
+      }
+  };
 
 const NavOptionsConfig =
   {
-    initialRouteName: 'SignIn',
-    backBehavior: 'order',
-  }
+  	initialRouteName: 'UserAuth',
+  	backBehavior: 'order',
+  };
 
 export default AuthStack = createSwitchNavigator(RouterConfig, NavOptionsConfig);
