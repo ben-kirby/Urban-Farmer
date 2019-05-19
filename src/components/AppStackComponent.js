ca12731627ca8cf8ea45905472c9272e915a1cf9
Ben Kirby <benkrby@gmail.com>
2019-05-18 21:26:50 -0700

    changed colors, fixed issues with export app stack

--- src/components/AppStackComponent.js
+++ src/components/AppStackComponent.js
@@ -7,31 +7,33 @@ import MainFooterMenuComponent from './MainFooterMenuComponent';
 
 const RouterConfig =
   {
-    Main:
+  	Main:
     {
-      screen: MainFooterMenuComponent,
-      navigationOptions:
+    	screen: MainFooterMenuComponent,
+    	navigationOptions:
       {
-        title: 'Urban Farmer',
-        headerStyle:
+      	title: 'Urban Farmer',
+      	headerStyle:
         {
-          backgroundColor: '#009900', //header background color
+          backgroundColor: '#f0f0f0', //header background color
         },
-        headerTitleStyle:
+      	headerTitleStyle:
         {
-          color: '#FFFFFF', //header text color
-          // fontFamily: 'serif', //Does not work on iOS
-          fontWeight: 'bold',
+        	color: '#000000', //header text color
+        	// fontFamily: 'serif', //Does not work on iOS
+        	fontWeight: 'bold',
         },
-        headerLeft: (<UpperLeftCornerIcon/>),
-        headerRight: (<SignOutButton/>),
+      	headerLeft: (<UpperLeftCornerIcon/>),
+      	headerRight: (<SignOutButton/>),
       },
     },
-  }
+  };
 
 //Note:  For style options see:  https://reactnavigation.org/docs/en/stack-navigator.html
 const NavOptionsConfig =
   {
-    initialRouteName: 'Main',
-  }
-export default AppStack = createStackNavigator(RouterConfig, NavOptionsConfig);
+  	initialRouteName: 'Main',
+  };
+let AppStack = createStackNavigator(RouterConfig, NavOptionsConfig);
+
+export default AppStack;
