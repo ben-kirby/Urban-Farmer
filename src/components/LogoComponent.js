228d02efc7d6bce3f9d154fa9786087249348048
Ben Kirby <benkrby@gmail.com>
2019-05-18 19:02:19 -0700

    removed view tags

--- src/components/LogoComponent.js
+++ src/components/LogoComponent.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { View, Image} from 'react-native';
+import { Image } from 'react-native';
 import Carrot from '../img/Carrot.png';
-import styles from '../styles/stylesComponent'
+import styles from '../styles/stylesComponent';
 
 export default function LogoComponent() {
 	return(
-		<View style={styles.authLogoContainer}>
-			<Image
-				style={styles.authLogoImage}
-				source={Carrot}
-			/>
-		</View>
+		<Image
+			style={styles.authLogoImage}
+			source={Carrot}
+		/>
 	);
 }
