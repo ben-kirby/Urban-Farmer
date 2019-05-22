4826de1eff13838f58f4cbaf86b3d466ab738e44
Ben Kirby <benkrby@gmail.com>
2019-05-22 14:09:38 -0700

    removed extra spaces

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -11,21 +11,14 @@ export default class ItemComponent extends Component {
     refresh: PropTypes.func
   };
 
-  
   render() {
       return(
         <View style={styles.itemCard}>
-
-          <Text style={styles.itemtext}>{this.props.product.name} </Text>
-      
+          <Text style={styles.itemtext}>{this.props.product.name}</Text>
           <View style={{alignItems:'center'}}>
-
           <SoldModal item={this.props.product}/> 
-          
-          <EditModal item={this.props.product} refresh={this.props.refresh}/>        
-
+          <EditModal item={this.props.product} refresh={this.props.refresh}/>
           </View>
-        
         </View>
     );
   }
