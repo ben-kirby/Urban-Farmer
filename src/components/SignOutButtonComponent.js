775c7f514680765a28eda7fc8acae05f2fdbffa1
Ben Kirby <benkrby@gmail.com>
2019-05-22 14:10:34 -0700

    changed how signout works. removed email

--- src/components/SignOutButtonComponent.js
+++ src/components/SignOutButtonComponent.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import AsyncStorage from '@react-native-community/async-storage';
+
+import { auth } from '../config';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 class SignOutButton extends Component {
 
   signUserOut = async () => {
     try {
-      await AsyncStorage.removeItem('uid').then(() => { 
-        AsyncStorage.removeItem('email')
-        }).then(() => { 
+      await AsyncStorage.removeItem('uid').then(() => {
+        auth.signOut().then(() => {
           this.props.navigation.navigate('AuthStack');
         });
+      });
     } catch (error) {
-      alert(error.message)
-      }
-  }
+      alert
+    }
+  };
 
   render() {
     return(
